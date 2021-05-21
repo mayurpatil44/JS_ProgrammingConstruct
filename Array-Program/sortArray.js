214952dbfafd092d73d61bb061cc8c9e87e6c2fa
mayurpatil44 <mayurpatil44@hotmail.com>
2021-05-21 22:43:45 +0530

    Array problem practice

---
+++ Array-Program/sortArray.js
@@ -0,0 +1,11 @@
+// 2.Sort the array.
+
+let numArray = new Array(10);
+for (let i = 0; i < numArray.length; i++) {
+  numArray[i] = Math.floor(Math.random() * 900) + 100;
+}
+let sortedArray = numArray.sort();
+console.log("Sorted array:");
+console.log(sortedArray);
+console.log("Using sorting\n2nd largest element: " + sortedArray[(sortedArray.length - 2)]
+  + "\n2nd smallest element: " + sortedArray[1]);
